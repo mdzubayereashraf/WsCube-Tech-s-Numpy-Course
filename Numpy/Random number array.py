e41c91d7c9c1e7160443e9e1f2029c0d52429095
mdzubayereashraf <mdzubayereashraf@gmail.com>
2026-06-17 14:18:12 +0600

    numpy

--- Numpy/Random number array.py
+++ Numpy/Random number array.py
@@ -9,3 +9,23 @@ print(var2)
 
 var3 = np.random.rand(2, 3, 4) # for three dimensional array with 2 blocks, 3 rows and 4 columns
 print(var3)
+
+#Creating a random number array between 0 to 10
+var4 = np.random.rand(4) * 10            
+print(var4)
+
+#Creating a random number array close to 0
+var5 = np.random.randn(4)
+print(var5)
+
+#creating a random number array close to zero
+var6 = np.random.randn(5, 6)
+print(var6)
+
+#crating a random number array between 0 and 10 with 3 rows and 4 columns
+var8 = np.random.randint(0, 10, size=(3, 4))
+print(var8)
+
+#crating a random number array between 0 and 10 with 3 rows and 4 columns
+var9 = np.random.randint(0, 10, 6).reshape(3, 2)
+print(var9)
